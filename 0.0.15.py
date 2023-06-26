58ed8b3b48e9ed3406466acc70a8a3e33cf08f01
Мкртумян Лусине Самвеловна <l.mkrtumyan@1c-ksu.ru>
2023-06-26 22:24:27 +0300

    0.0.15 Операторы ввода и вывода.

---
+++ 0.0.15.py
@@ -0,0 +1,14 @@
+print("Hello world!")
+print(1, 2, 3)
+print(2**8)
+
+print('Как вас зовут?')
+name = input()
+print('Здравствуйте, ' + name + '!')
+print(type(name))
+
+age_str = input("Введите ваш возраст: ")
+age_int = int(age_str)
+print(age_int)
+print(type(age_str))
+print(type(age_int))
