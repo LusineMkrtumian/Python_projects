48dc1b8defda0017e62d36506ff7a2d2a7ff6f6a
Мкртумян Лусине Самвеловна <l.mkrtumyan@1c-ksu.ru>
2023-06-26 22:41:08 +0300

    0.0.20 Словари.

---
+++ 0.0.20.py
@@ -0,0 +1,10 @@
+python_words = {'list': 'A collection of values that are not connected, but have an order.',
+                'dictionary': 'A collection of key-value pairs.',
+                'function': 'A named set of instructions that defines a set of actions in Python.'}
+print(python_words)
+print("\nWord: %s" % 'list')
+print("Meaning: %s" % python_words['list'])
+print("\nWord: %s" % 'dictionary')
+print("Meaning: %s" % python_words['dictionary'])
+print("\nWord: %s" % 'function')
+print("Meaning: %s" % python_words['function'])
