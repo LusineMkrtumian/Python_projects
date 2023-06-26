862f750871fe24e526cf8b414fbc578b34e65c45
Мкртумян Лусине Самвеловна <l.mkrtumyan@1c-ksu.ru>
2023-06-26 22:20:34 +0300

    0.0.13 Конкатенация строк.

---
+++ 0.0.13.py
@@ -0,0 +1,5 @@
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name + ' ' + last_name
+message = full_name.title() + ' ' + "was considered the world's first computer programmer."
+print(message)
