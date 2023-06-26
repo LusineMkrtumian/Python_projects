82b1a6997aca4e1bbb655d69f9fc4f5b0ff9e879
Мкртумян Лусине Самвеловна <l.mkrtumyan@1c-ksu.ru>
2023-06-26 22:17:27 +0300

    0.0.11 Строковый тип данных.

---
+++ 0.0.11.py
@@ -0,0 +1,5 @@
+my_string = "This is a double-quoted string."
+my_string = 'This is a single-quoted string.'
+
+quote = "Лао Цзы однажды сказал: 'Лучшее знание — это незнание о том, что ты что-то знаешь.'"
+print(quote)
