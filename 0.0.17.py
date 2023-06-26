6bdd7a9ad22d4d5855488883e5142c1f1c74feac
Мкртумян Лусине Самвеловна <l.mkrtumyan@1c-ksu.ru>
2023-06-26 22:35:15 +0300

    0.0.17 Списки (lists).

---
+++ 0.0.17.py
@@ -0,0 +1,14 @@
+users = ["cotique", "miptgirl", "yorko", "vradchenko"]
+ages = [18, 25, 32, 30]
+print(users)
+print(ages)
+print(ages[0])
+print(ages[0], ages[2])
+# ages[8] - error
+print(ages[len(ages) - 1])
+print(ages[-2])
+
+mix = [18, "Oslo", 25, "New York", 32, "London", 30, "Havana"]
+print(mix)
+print(len(mix))
+
