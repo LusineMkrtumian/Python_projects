108be86fc04f943dfcdef795d6251368c2895594
Мкртумян Лусине Самвеловна <l.mkrtumyan@1c-ksu.ru>
2023-06-27 21:01:11 +0300

    0.2.5 Функция range().

---
+++ 0.2.5.py
@@ -0,0 +1,16 @@
+# range(fin) возвращает последовательность чисел от 0 до f in − 1 с шагом 1;
+# range(start, fin) возвращает последовательность от start до f in − 1 с шагом 1;
+# range(start, fin, step) возвращает последовательность от start до f in − 1 с шагом step
+
+for i in range(3):
+    print(i)
+
+for i in range(0, 10):
+    print(i)
+
+for i in range(0, 8, 2):
+    print(i)
+
+for i in range(10, 0, -1):
+    print(i)
+
