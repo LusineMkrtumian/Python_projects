177b7d94c000075c822a3f76f5399aa6bf49af12
Мкртумян Лусине Самвеловна <l.mkrtumyan@1c-ksu.ru>
2023-07-01 22:46:29 +0300

    Pandas_1.2.

---
+++ Pandas_1.2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+columns = ['Year', 'Movie name', 'Rating', 'Votes', 'Genre', 'Runtime', "Director"]
+
+data = [['2015', 'Bajirao Mstani', '7.2', '17362', 'Dramma', '158', 'Sanjay Leela Bhamsali'],
+        ['2014', 'Qeen', '8.4', '39518', 'Dramma', '146', 'Vikas Bahl'],
+        ['2013', 'Bhaag Mikha Bhaag', '8.3', '39731', 'Dramma', '186', 'Rakeysh omprakash'],
+        ['2012', 'Barfi!', '8.2', '52308', 'Familiy', '151', 'Anurag Basu']]
+
+df = pd.DataFrame(data=data, index=None, columns=columns, dtype=None, copy=None)
+
+print(df.loc[df['Genre'] == 'Dramma'])
